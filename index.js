7cb8dd72a68d7bf8be14b9db645ec4a20fd7732a
Skye <jiawen.liang@accenture.com>
2022-01-04 14:19:22 +0800

    express

--- index.js
+++ index.js
@@ -15,33 +15,42 @@
 // console.log(process.env.VERSION);
 
 
-const http = require('http');
-const { waitForDebugger } = require('inspector');
+// const http = require('http');
+// const { waitForDebugger } = require('inspector');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    console.log(`before wait: ${new Date()}`);
-    wait(10);
-    console.log(`after wait: ${new Date()}`);
-    res.end('Hello world');
+// const server = http.createServer((req, res)=>{
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     console.log(`before wait: ${new Date()}`);
+//     wait(10);
+//     console.log(`after wait: ${new Date()}`);
+//     res.end('Hello world');
+// })
+
+// const wait = async () =>{
+//     console.log(`before sleep:${new Date()}`);
+//     await sleep(10);
+//     console.log(`after sleep:${new Date()}`);
+// }
+
+// const sleep = async(seconds)=>{
+//     return new Promise(function(resolve){
+//         setTimeout(resolve, seconds*1000);
+//     })
+// }
+
+// server.listen(port,hostname,()=>{
+//     console.log(`Server running at http://${hostname}:${port}`)
+// })
+
+const express = require('express')
+const app = express();
+ 
+app.get('/',function(req,res){
+    res.send('hello world')
 })
 
-const wait = async () =>{
-    console.log(`before sleep:${new Date()}`);
-    await sleep(10);
-    console.log(`after sleep:${new Date()}`);
-}
-
-const sleep = async(seconds)=>{
-    return new Promise(function(resolve){
-        setTimeout(resolve, seconds*1000);
-    })
-}
-
-server.listen(port,hostname,()=>{
-    console.log(`Server running at http://${hostname}:${port}`)
-})
+app.listen(3000);
